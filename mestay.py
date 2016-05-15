139a4c0d954d499e764b0e6e65867874c27deba9
nidhog <ismail.elouafiq@telecom-bretagne.eu>
2016-05-15 22:17:49 +0200

    shape/head read

--- mestay.py
+++ mestay.py
@@ -1,1 +1,17 @@
-import pandas
+import pandas as pd
+
+destionations = pd.read_csv("data/destinations.csv")
+test = pd.read_csv("data/test.csv")
+train = pd.read_csv("data/train.csv")
+print "[>]  SHAPE"
+print "[.] Destinations"
+print destinations.shape()
+print "[.] Test"
+print test.shape()
+print "[.] Train"
+print train.shape()
+print "[>]  HEAD"
+print "[.] Train"
+print train.head(5)
+print "[.] Test"
+print test.head(5)
